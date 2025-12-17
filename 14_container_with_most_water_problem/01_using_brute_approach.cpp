f601c57eb39508aa23af9ab3c5d4dff5d904b2eb
Hanif <mohammadabuhanif95@gmail.com>
2025-12-17 16:10:49 +0800

    Container With Most Water solutions
    - 01 using O(n²) brute force solution
    - 02_using O(n) two-pointer optimal solution

---
+++ 14_container_with_most_water_problem/01_using_brute_approach.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ int maxArea(vector<int>& height){
+    int maxWater = 0;
+
+    for(int i = 0; i < height.size(); i++){
+        for(int j = i+1; j < height.size(); j++){
+            int w = j - i;
+            int ht = min(height[i], height[j]);
+            int currWater = ht * w;
+
+            maxWater = max(maxWater, currWater);
+        }
+    }
+    return maxWater;
+ }
+
+ int main(){
+    vector<int> height = { 1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+    cout <<"Max area: " << maxArea(height)<< endl;
+    return 0;
+ }
